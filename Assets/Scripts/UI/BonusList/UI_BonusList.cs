d3971a57510857c903adaee5a7b7fec751054227
alvina-dr <alvinadr24@gmail.com>
2025-07-04 11:40:10 +0200

    Fix : bonus list

--- Assets/Scripts/UI/BonusList/UI_BonusList.cs
+++ Assets/Scripts/UI/BonusList/UI_BonusList.cs
@@ -1,16 +1,26 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class UI_BonusList : MonoBehaviour
 {
     [SerializeField] private UI_BonusIcon _bonusIconPrefab;
     [SerializeField] private Transform _bonusIconParent;
+    [SerializeField] private List<UI_BonusIcon> _bonusIconList = new();
 
     public void UpdateBonusList()
     {
+        for (int i = _bonusIconList.Count - 1; i >= 0; i--)
+        {
+            Destroy(_bonusIconList[i].gameObject);
+        }
+
+        _bonusIconList.Clear();
+
         for (int i = 0; i < GameManager.Instance.BonusList.Count; i++)
         {
             UI_BonusIcon bonusIcon = Instantiate(_bonusIconPrefab, _bonusIconParent);
             bonusIcon.Setup(GameManager.Instance.BonusList[i]);
+            _bonusIconList.Add(bonusIcon);
         }
     }
 }
