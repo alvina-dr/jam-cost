f6240549d4dd7a7b7d41151e838fb70b3cd5e462
Alvina <alvinadr24@gmail.com>
2025-12-19 20:39:01 +0100

    Add : pause

---
+++ Assets/Scripts/PauseManager.cs
@@ -0,0 +1,58 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PauseManager : MonoBehaviour
+{
+    #region Singleton
+    public static PauseManager Instance { get; private set; }
+
+    private void Awake()
+    {
+
+        if (Instance != null && Instance != this)
+        {
+            Destroy(this);
+        }
+        else
+        {
+            Instance = this;
+            SetupActionInput();
+        }
+    }
+    #endregion
+
+    public bool IsPaused { get; private set; }
+
+    [SerializeField] private UI_Menu _menu;
+
+    public void SetupActionInput()
+    {
+        InputAction pauseAction = InputSystem.actions.FindAction("Pause");
+        pauseAction.performed += Pause;
+    }
+
+    public void Pause(InputAction.CallbackContext _)
+    {
+        IsPaused = !IsPaused;
+        if (IsPaused)
+        {
+            Time.timeScale = 0f;
+            OpenMenu();
+        }
+        else
+        {
+            Time.timeScale = 1f;
+            CloseMenu();
+        }
+    }
+
+    public void OpenMenu()
+    {
+        _menu.OpenMenu();
+    }
+
+    public void CloseMenu()
+    {
+        _menu.CloseMenu();
+    }
+}
