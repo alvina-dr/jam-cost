9c1bb53c2f1e0772e134bf67ff518c0100501927
Alvina <alvinadr24@gmail.com>
2026-02-12 17:52:43 +0100

    Add : UI permanent shop WIP

---
+++ Assets/Scripts/Hub/PermanentBonusShop/UI_PermanentBonusShop.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+public class UI_PermanentBonusShop : UI_Menu
+{
+    public override void OpenMenu()
+    {
+        base.OpenMenu();
+    }
+
+    public override void CloseMenu()
+    {
+        base.CloseMenu();
+    }
+}
