f28dda64b6acad58b90565793d294c53b3d62de1
Alvina <alvinadr24@gmail.com>
2026-03-02 15:34:06 +0100

    Add : ending scene

---
+++ Assets/Scripts/EndingManager.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class EndingManager : MonoBehaviour
+{
+    [SerializeField] private string _endingDialog;
+
+
+    private void Start()
+    {
+        DialogueManager.Instance.DialogueRunner.StartDialogue(_endingDialog);
+        DialogueManager.Instance.EndDialogueEvent += CallEndDialogueEvent;
+
+    }
+
+    public void CallEndDialogueEvent()
+    {
+        SaveManager.Instance.AddMT(3);
+        SceneManager.LoadScene("Hub");
+    }
+
+}
