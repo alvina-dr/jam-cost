722ad5b2856334cfab466ed924d6ce847a403c5a
Alvina <alvinadr24@gmail.com>
2026-04-16 17:24:11 +0200

    Add : use power gain 1 PP

--- Assets/Scripts/ScriptableObjects/Bonus/BD_PowerChargeSpeed.cs
+++ Assets/Scripts/ScriptableObjects/Bonus/BD_PowerChargeSpeed.cs
@@ -3,8 +3,5 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "BD_PowerChargeSpeed", menuName = "Scriptable Objects/Bonus/BD_PowerChargeSpeed")]
 public class BD_PowerChargeSpeed : BonusData
 {
-    public override void GetBonus()
-    {
-        base.GetBonus();
-    }
+
 }
